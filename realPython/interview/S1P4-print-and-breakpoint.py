5fd31d0ce95477b5ec74c859d92953e70a71de24
James Harding <jt.harding70@gmail.com>
2024-02-10 00:37:48 -0600

    python interview code

---
+++ realPython/interview/S1P4-print-and-breakpoint.py
@@ -0,0 +1,10 @@
+def max(lst):
+    max_num = -float('inf')
+    for num in lst:
+        breakpoint()
+        if num > max_num:
+            max_num = num
+    return max_num
+
+
+print(max([-1, -2, -4]))
